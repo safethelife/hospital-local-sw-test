d487befbc735f31ecc802f5698388febd1a723be
JungHo-K1m <qopqop55@naver.com>
2024-04-08 17:20:41 +0900

    feature/#2#3_upload_files_to_AWS

--- hospital_SW/app.py
+++ hospital_SW/app.py
@@ -8,17 +8,43 @@ from watchdog.events import FileSystemEventHandler
 import threading
 from queue import Queue, Empty
 import pydicom
+import boto3
+from botocore.exceptions import NoCredentialsError
 import os
+from env import settings
 
 
 
 def extract_patient_info(dicom_file_path):
     ds = pydicom.dcmread(dicom_file_path)
-    patient_name = ds.PatientName if 'PatientName' in ds else 'Unknown'
-    patient_id = ds.PatientID if 'PatientID' in ds else 'Unknown'
-    patient_birth_date = ds.PatientBirthDate if 'PatientBirthDate' in ds else 'Unknown'
-    patient_sex = ds.PatientSex if 'PatientSex' in ds else 'Unknown'
+    patient_name = str(ds.PatientName) if 'PatientName' in ds else 'Unknown'
+    patient_id = str(ds.PatientID) if 'PatientID' in ds else 'Unknown'
+    patient_birth_date = str(ds.PatientBirthDate) if 'PatientBirthDate' in ds else 'Unknown'
+    patient_sex = str(ds.PatientSex) if 'PatientSex' in ds else 'Unknown'
     return patient_name, patient_id, patient_birth_date, patient_sex
+
+
+def upload_to_s3(file_name, bucket, object_name=None, metadata=None):
+    if object_name is None:
+        object_name = file_name
+    if metadata is None:
+        metadata = {}
+
+    # S3 클라이언트 생성
+    s3_client = boto3.client('s3',
+                             aws_access_key_id=settings.AWS['access_key_id'],
+                             aws_secret_access_key=settings.AWS['secret_access_key'])
+
+    try:
+        # 파일 내용 읽기
+        with open(file_name, 'rb') as file_data:
+            s3_client.put_object(Bucket=bucket, Key=object_name, Body=file_data, Metadata=metadata)
+        print(f"File {object_name} uploaded successfully with metadata.")
+    except NoCredentialsError:
+        print("Credentials not available")
+        return False
+    return True
+
 
 
 class FileEventHandler(FileSystemEventHandler):
@@ -44,24 +70,24 @@ class FileEventHandler(FileSystemEventHandler):
                 time.sleep(self.buffer_timeout)
                 current_time = time.time()
                 if current_time - self.last_event_time >= self.buffer_timeout:
-                    # 타임아웃이 지나면 로그 출력
-                    messages = []
+                    # 타임아웃이 지나면 로그 출력 및 파일 업로드
                     try:
                         while True:
                             # 큐에서 파일 경로를 가져와 환자 정보를 추출
                             file_path = self.buffer.get_nowait()
                             patient_info = extract_patient_info(file_path)
                             message = f"New DICOM file: {os.path.basename(file_path)}, Patient Name: {patient_info[0]}, Patient ID: {patient_info[1]}, Birth Date: {patient_info[2]}, Sex: {patient_info[3]}"
-                            messages.append(message)
+
+                            # 파일을 S3에 업로드하면서 환자 이름과 ID를 메타데이터로 추가
+                            metadata = {
+                                'patient_name': patient_info[0],
+                                'patient_id': patient_info[1]
+                            }
+                            upload_to_s3(file_path, settings.AWS['bucket'], os.path.basename(file_path), metadata)
                     except Empty:
                         pass
-
-                    if messages:
-                        log_message = "\n".join(messages)
-                        self.signal.emit(log_message)
             except Exception as e:
                 print(f"Error in process_buffer: {e}")
-
 
 
 class DirectoryMonitorThread(QThread):
